61d19cd3f5a43e6c95d104c8562b34cb786499eb
lblenner <louis.blenner@gmail.com>
2023-10-01 17:57:23 +0200

    color picker use room colors

--- src/components/dessiner/ColorPicker.svelte
+++ src/components/dessiner/ColorPicker.svelte
@@ -1,29 +1,34 @@
 <script lang="ts">
-	import { toolColor } from './tools';
+  import { selectedRoom } from "../appState";
+  import { toolColor } from "./tools";
 
-	let colors = ['#50d71e', '#9625b3', '#000000', '#c22d00', '#1ff0ec', '#ffffff'];
+  let colors = $selectedRoom?.colors;
 
-	let selected_color = colors[0];
-	toolColor.update(() => selected_color);
+  let selected_color = colors ? colors[0] : null;
+  toolColor.update(() => selected_color ?? "");
 
-	toolColor.subscribe((value) => {
-		selected_color = value
-	})
+  toolColor.subscribe((value) => {
+    selected_color = value;
+  });
 </script>
 
 <div class="w-[144px] flex items-center justify-center flex-col mt-4">
-	<div class="grid grid-cols-3 gap-2 mb-2">
-		{#each colors as color, i}
-			<button
-				class=" w-[30px] h-[30px] {selected_color === color
-					? 'border-green-600 border-4'
-					: 'border-black border-2'}"
-				style="background-color : {color};"
-				on:click={() => {
-					toolColor.update(() => color);
-				}}
-			/>
-		{/each}
-	</div>
-	{selected_color}
+  <div class="grid grid-cols-3 gap-2 mb-2">
+    {#if colors}
+      {#each colors as color, i}
+        <button
+          class=" w-[30px] h-[30px] {selected_color === color
+            ? 'border-green-600 border-4'
+            : 'border-black border-2'}"
+          style="background-color : {color};"
+          on:click={() => {
+            toolColor.update(() => color);
+          }}
+        />
+      {/each}
+	  {:else}
+		  No colors
+    {/if}
+  </div>
+  {selected_color}
 </div>
